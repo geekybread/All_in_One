362f36d25f1054e0db69342f91ad29a864e83c58
geeky_bread <abarwalcu@gmail.com>
2022-01-08 12:57:53 +0530

    updated worker

--- worker.py
+++ worker.py
@@ -1,13 +1,16 @@
 import os
-
+import urllib.parse
 import redis
+from redis import Redis
 from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
 
 redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 
-conn = redis.from_url(redis_url)
+urllib.parse.uses_netloc.append('redis')
+url = urllib.parse.urlparse(redis_url)
+conn = Redis(host=url.hostname, port=url.port, db=0, password=url.password)
 
 if __name__ == '__main__':
     with Connection(conn):
